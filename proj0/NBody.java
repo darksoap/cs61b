af3e270e36b80ac9f1c91499a74b2531dfc29ef2
darksoap <darksoap@foxmail.com>
2018-11-03 19:34:52 +0800

    finish proj0

--- proj0/NBody.java
+++ proj0/NBody.java
@@ -40,11 +40,35 @@ public class NBody{
             p.draw();
         }
 
-        StdDraw.show();
         StdDraw.pause(2000);
 
-        enableDoubleBuffering();
+        StdDraw.enableDoubleBuffering();
         double time = 0;
-        
+        double [] xForces = new double[planets.length];
+        double [] yForces = new double[planets.length];
+        while (time < T){
+            for (int i = 0; i < planets.length; i++){
+                xForces[i] = planets[i].calcNetForceExertedByX(planets);
+                yForces[i] = planets[i].calcNetForceExertedByY(planets);
+            }
+            for (int i = 0; i < planets.length; i++) {
+				planets[i].update(dt, xForces[i], yForces[i]);
+            }
+            StdDraw.picture(0, 0, "images/starfield.jpg");
+			for (Planet p : planets) {
+				p.draw();
+            }
+            StdDraw.show();
+            StdDraw.pause(10);
+            time += dt;
+        }
+
+        StdOut.printf("%d\n", planets.length);
+        StdOut.printf("%.2e\n", radius);
+        for (int i = 0; i < planets.length; i++) {
+            StdOut.printf("%11.4e %11.4e %11.4e %11.4e %11.4e %12s\n",
+                        planets[i].xxPos, planets[i].yyPos, planets[i].xxVel,
+                        planets[i].yyVel, planets[i].mass, planets[i].imgFileName);   
+        }
     }
 }
